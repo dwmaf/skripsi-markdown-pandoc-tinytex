38841731049ced0a627d2075b38f59a4155c0abd
nana72848e <d1041211005@student.untan.ac.id>
2026-03-02 14:34:17 +0700

    pisahkan jadi per bab

--- generate.bat
+++ generate.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Sedang membuat PDF...
-pandoc metadata.yaml laporan-skripsi.md --citeproc -o Hasil_Skripsi.pdf --pdf-engine=xelatex --number-sections
+pandoc metadata.yaml 01-bab1.md 02-bab2.md 03-bab3.md 04-bab4.md 05-bab5.md --citeproc -o Hasil_Skripsi.pdf --pdf-engine=xelatex --number-sections
 echo.
 echo SELESAI! Cek file Hasil_Skripsi.pdf
 pause
