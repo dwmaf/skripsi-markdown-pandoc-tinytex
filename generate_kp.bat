b50b1b301d588dd2ff1cb70182057e13d3354347
nana72848e <d1041211005@student.untan.ac.id>
2026-03-10 10:59:58 +0700

    add simbol for class, use case, erd

--- generate_kp.bat
+++ generate_kp.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Sedang membuat PDF...
-pandoc metadata.yaml kp/kp-bab1.md kp/kp-bab2.md kp/kp-bab3.md kp/kp-bab4.md kp/kp-bab5.md --citeproc -o Hasil_KP.pdf --pdf-engine=xelatex --number-sections
+pandoc metadata.yaml kp/kp-bab1.md kp/kp-bab2.md kp/kp-bab3.md kp/kp-bab4.md kp/kp-bab5.md --citeproc -o DawamAgungFathoni_Laporan_KP.pdf --pdf-engine=xelatex --number-sections
 echo.
-echo SELESAI! Cek file Hasil_KP.pdf
+echo SELESAI! Cek file DawamAgungFathoni_Laporan_KP.pdf
 pause
